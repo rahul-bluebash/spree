d875a787122186b5fea528e09a3076c661edb529
Damian Legawiec <damian@sparksolutions.co>
2021-03-30 22:51:53 +0200

    Update base_serializer.rb

--- api/app/serializers/spree/api/v2/platform/base_serializer.rb
+++ api/app/serializers/spree/api/v2/platform/base_serializer.rb
@@ -2,7 +2,7 @@ module Spree
   module Api
     module V2
       module Platform
-        class BaseSerializer < ::Spree::V2::BaseSerializer
+        class BaseSerializer < ::Spree::Api::V2::BaseSerializer
         end
       end
     end
